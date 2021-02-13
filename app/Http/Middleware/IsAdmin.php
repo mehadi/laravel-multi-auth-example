1b1ce85cbef01b5793e58598136df1fd1259f459
Mehadi Hasan <bdmehadih@gmail.com>
2021-02-13 12:10:31 +0600

    simple update

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -7,13 +7,6 @@ use Illuminate\Http\Request;
 
 class IsAdmin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
     public function handle($request, Closure $next)
     {
         if(auth()->user()->is_admin == 1){
