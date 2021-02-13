9e5290e2493cfcaafcdac722bfdfb1e6a3e8d059
Mehadi Hasan <bdmehadih@gmail.com>
2021-02-13 11:52:30 +0600

    project upload

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $user = Auth::user();
+        if ($user->is_admin) {
+            return view('adminDashboard');
+        }
+        return view('home');
+    }
+
+}
